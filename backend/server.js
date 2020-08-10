f123b25582677a351f268110abcc1b2b5fa75cc4
BigBrainSmolBoi <najamiftikhar2020@gmail.com>
2020-08-10 10:11:41 +0500

    setting cross origin headers

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
 const express = require('express');
+const uuid = require('uuid');
+
+const app = express();
+
+const DUMMY_PRODUCSTS = [];
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
+  next();
+});
